95c96f55a715cad03af9a690072926921d386284
Prateek1337 <prateeksahu1337@gmail.com>
2019-08-11 11:20:52 +0530

    Modified client4.c

--- Week0/client4.c
+++ Week0/client4.c
@@ -8,6 +8,7 @@
 #include <netdb.h> 
 #include<time.h>
 #include<errno.h>
+#include <arpa/inet.h>
 
 void error(const char *msg)
 {
@@ -93,7 +94,6 @@ int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
-    struct hostent *server;
 
 
     if (argc < 3) {
@@ -104,16 +104,13 @@ int main(int argc, char *argv[])
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    }
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
